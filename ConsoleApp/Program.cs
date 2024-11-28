ed501c14390a4e5dfad7d9b4ee25a1809871e7b6
szvidzwa <szvidzwa@css.edu>
2024-11-27 21:43:04 -0600

    Assignment 12 Done!

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,16 +1,26 @@
-﻿using SubtypeCovariance;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SubtypeCovariance;
 
-class Program
+namespace ConsoleSample
 {
-    static void Main(string[] args)
+    class Program
     {
+        static void Main(string[] args)
+        {
 
-        EntityRepository entityRepository = new EntityRepository();
-        Entity myEntity = entityRepository.GetByID(Guid.NewGuid());
+            UserRepository entityRepository = new UserRepository();
+            Entity myEntity = entityRepository.GetByID(Guid.NewGuid());
 
-        myEntity.Name = "Tom";
-        Console.WriteLine("New entity ID: " + myEntity.ID.ToString());
-        Console.WriteLine("New entity Name: " + myEntity.Name);
+            myEntity.Name = "Simba";
+            Console.WriteLine("New entity ID: " + myEntity.ID.ToString());
+            Console.WriteLine("New entity Name: " + myEntity.Name);
 
+            Console.WriteLine("Press any key to quit...");
+            Console.ReadKey();
+        }
     }
 }
